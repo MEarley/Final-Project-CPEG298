58654a13bc85fae723e2892e83c1d805ba16b1b0
MEarley <113404793+MEarley@users.noreply.github.com>
2023-03-15 17:22:42 -0400

    KiCad & Code
    -Labeling on PCB
    -Preparing code for millivolt reader

--- SMART-MFC_Code/8266demo-rdm/8266demo-rdm.ino
+++ SMART-MFC_Code/8266demo-rdm/8266demo-rdm.ino
@@ -1,6 +1,3 @@
-#include <DHT.h>
-#include <DHT_U.h>
-
 /***************************************************************************
   This is an example program for the sending a counter to Adafruit IO using
   an ESP8266 WiFi module.  You will need to correct the WiFi SSID and password
@@ -11,20 +8,44 @@
  ***************************************************************************/
 #include "Arduino.h"
 #include <SoftwareSerial.h>		          //Allows us to use two GPIO pins for a second UART
+#include "Seeed_BMP280.h"
+#include <Wire.h>
+//#include <DHT.h>
+//#include <DHT_U.h>
 #define TEMPPIN D3
+#include <U8g2lib.h>
+
+
+// u8g2 Library Definitions
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+//#include <Wire.h>
+#endif
+
+//U8G2_SSD1306_128X64_ALT0_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // SSD1306 and SSD1308Z are compatible
+ 
+// U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //Low spped I2C
+// End of u8g2
+
+
 SoftwareSerial espSerial(11,10);	      //Create software UART to talk to the ESP8266
+BMP280 BMP;
 String IO_USERNAME = "MEarley";
 String IO_KEY  =     "aio_kdyu82j8scwf9zQ65mE348JAyuWq";
 String WIFI_SSID = "UD Devices"; 	    //Only need to change if using other network, eduroam won't work with ESP8266
 String WIFI_PASS = ""; 		            //Blank for open network
 float num = 1.0; 			                  //Counts up to show upload working
 float temperature;
+int lightLevel;
+float humidity;
 
 // DHT pin 3 type DHT11
-  DHT dht(3, 11);
+  //DHT dht(6, 11);
 
 void setup() {
-	Serial.begin(9600);		// set up serial monitor with 9600 baud rate
+	Serial.begin(9600);		// set up Serial monitor with 9600 baud rate
 	espSerial.begin(9600);		// set up software UART to ESP8266 @ 9600 baud rate
 	Serial.println("setting up");
 	String resp = espData("get_macaddr",2000,true);	//get MAC address of 8266
@@ -38,12 +59,23 @@ void setup() {
 		while(1);
 	}
 	resp = espData("setup_feed=1,CPEG-ELEG298",2000,false);	//start the data feed
+
+  // Initiate BMP280
+  if(!BMP.init()){
+    Serial.println("Failed to initate BMP280");
+  } else{
+    Serial.println("Successfully initated BMP280");
+  }
 	
   // Sets pin 3 to output
-  pinMode(3, OUTPUT);
-  temperature = digitalRead(3);
+  pinMode(6, OUTPUT);
+  lightLevel = analogRead(6);
   
-  dht.begin();
+  //dht.begin();
+  //u8g2.begin();
+
+  // Millivolt Reader
+  
 
   Serial.println("------ Setup Complete ----------");
 }
@@ -52,30 +84,55 @@ void loop() {
 
 	// free version of Adafruit IO only allows 30 uploads/minute, it discards everything else
 	delay(5000);			// Wait 5 seconds between uploads
-	Serial.print("Num is: ");  
-	Serial.println(num);
+	//Serial.print("Num is: ");  
+	//Serial.println(num);
   
-	String resp = espData("send_data=1,"+String(num),2000,false); //send feed to cloud
-	num = num +0.5;			// Count by 0.5 increments
+	
+	//num = num +0.5;			// Count by 0.5 increments
 
-  Serial.print("Attempting to Read from pin 3\n");
-  temperature = digitalRead(3);  
-  Serial.println(temperature);
+  Serial.print("Attempting to read light levels from pin 6\n");
+  lightLevel = analogRead(6);  
+  Serial.println(lightLevel);
+
+  Serial.print("Attempting to read temperature from BMP280\n");
+  temperature = BMP.getTemperature();  
+  Serial.print(temperature);
+  Serial.println(" C");
+
+  //Serial.print("Attempting to read humidity from BMP280\n");
+  //humidity = BMP.getHumidity();  
+  //Serial.print(humidity);
+  //Serial.println(" UNIT");
+
+  /*
 
   Serial.print("Attempting to read temperature from function\n");
   temperature = dht.readTemperature(false);
-  Serial.println(temperature);
+  Serial.println(temperature);*/
+/*
+  // u8g2 Display
+  u8g2.clearBuffer();                   // clear the internal memory
+  u8g2.setFlipMode(1);
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+  //char *TemperatureString;
+  //TemperatureString = malloc(sizeof(char) * 30);
+  //sprintf(TemperatureString,"P: %.2f C",25.3423);
+  //u8g2.drawStr(0,10,TemperatureString);    // write something to the internal memory
+  u8g2.sendBuffer();                    // transfer internal memory to the display
+  // End of Display    */
+
+  String resp = espData("send_data=1,"+String(temperature),2000,false); //send feed to cloud
 }
 
 String espData(String command, const int timeout, boolean debug) {
 	String response = "";
-	espSerial.println(command);	//send data to ESP8266 using serial UART
+	espSerial.println(command);	//send data to ESP8266 using Serial UART
 	long int time = millis();
 	while ( (time + timeout) > millis()) {	//wait the timeout period sent with the command
 		while (espSerial.available()) {	//look for response from ESP8266
 			char c = espSerial.read();
 			response += c;
-			Serial.print(c);	//print response on serial monitor
+			Serial.print(c);	//print response on Serial monitor
 		}
 	}
 	if (debug) {
